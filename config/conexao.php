207884d5931b9f410a5739b2299a3d7c4aedfd36
NetoRBL <52009812+NetoRBL@users.noreply.github.com>
2019-08-22 15:03:06 -0300

    Initial commit

---
+++ config/conexao.php
@@ -0,0 +1,23 @@
+<?php
+
+  class Conexao {
+
+    private $host = "localhost";
+    private $usuario = "root"; 
+    private $senha = "";
+    private $nomeBanco = "modesto";
+
+    public function conecta(){
+      try{
+       $conexao = new PDO('mysql:host='.$this->host.';dbname='.$this->nomeBanco.';charset=utf8', $this->usuario, $this->senha);
+       return $conexao;
+      } 
+      catch(PDOException $e){
+        //echo $e->getMessage();
+        return false;
+      }
+    }
+  }
+$conexao = new Conexao();
+$pdo = $conexao->conecta();
+?>
