ffda5a58c7fb6d68a619f99be82ebdc8599b2b1d
LucasWar <54415418+LucasWar@users.noreply.github.com>
2019-08-23 11:55:08 -0300

    teste
    Co-Authored-By: netorbl <netorbl@users.noreply.github.com>

--- view/pages/realizarVenda.php
+++ view/pages/realizarVenda.php
@@ -116,54 +116,22 @@
 
     <!-- Main content -->
     <section class="content">
-      <!-- Small boxes (Stat box) -->
-  
-      <!-- /.row -->
-      <!-- Main row -->
-      <div class="row">
+    <div class=" col-md-7">
+      <select class="form-control form-control-sm ">
+      <option>Large select</option> 
+
+    </select>
+    </div>
+    
+
         <!-- Left col -->
-        <section class="col-lg-7 connectedSortable">
-          <!-- Custom tabs (Charts with tabs)-->
-          <div class="nav-tabs-custom">
-            <!-- Tabs within a box -->
-            <ul class="nav nav-tabs pull-right">
-              <li class="active"><a href="#revenue-chart" data-toggle="tab">Area</a></li>
-              <li><a href="#sales-chart" data-toggle="tab">Donut</a></li>
-              <li class="pull-left header"><i class="fa fa-inbox"></i> Sales</li>
-            </ul>
-            <div class="tab-content no-padding">
-              <!-- Morris chart - Sales -->
-              <div class="chart tab-pane active" id="revenue-chart" style="position: relative; height: 300px;"></div>
-              <div class="chart tab-pane" id="sales-chart" style="position: relative; height: 300px;"></div>
-            </div>
-          </div>
-          <!-- /.nav-tabs-custom -->
+        <section >
+            
+              
+            
         </section>
 
-        <!-- right col -->
-        <section class="col-lg-5 connectedSortable">
-          <!-- Calendar -->
-          <div class="box box-solid bg-green-gradient">
-            <div class="box-header">
-              <i class="fa fa-calendar"></i>
-
-              <h3 class="box-title">Calendar</h3>
-              <!-- tools box -->
-              <div class="pull-right box-tools">
-                <!-- button with a dropdown -->
-                <button type="button" class="btn btn-success btn-sm" data-widget="collapse"><i class="fa fa-minus"></i>
-                </button>
-              </div>
-              <!-- /. tools -->
-            </div>
-            <!-- /.box-header -->
-            <div class="box-body no-padding">
-              <!--The calendar -->
-              <div id="calendar" style="width: 100%"></div>
-            </div>
-            <!-- /.box-body -->
-          </div>
-          <!-- /.box -->
+        
         </section>
       </div>
       <!-- /.row (main row) -->
